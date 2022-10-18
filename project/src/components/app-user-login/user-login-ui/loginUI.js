5bfd689842fce2b6d2502c8a752f528ad076ed5a
Gravgor <36999577+Gravgor@users.noreply.github.com>
2022-10-18 02:18:36 +0200

    Update loginUI.js

--- project/src/components/app-user-login/user-login-ui/loginUI.js
+++ project/src/components/app-user-login/user-login-ui/loginUI.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-
 import './loginUI.css'
+import { Typography } from '@mui/material';
 
 const LoginUI = () => {
 
@@ -17,7 +17,21 @@ const LoginUI = () => {
             <ArrowForwardIcon style={{position: 'relative', top: '5px'}}/>
         </div>
         <p>Skip</p>
+    </div>
+    <div className='login-content'>
+        <div className='login-content-header' style={{width: '250px', display: 'flex', flexDirection: 'column', marginTop: '65px', marginLeft: '35px'}}>
+            {userStep === 1 &&
+            <>
+             <Typography variant='p' style={{fontWeight: 'bold', fontSize: '30px'}}>Hello! 🤗</Typography>
+             <Typography variant='p' style={{fontWeight: 'bold', fontSize: '30px'}}>Let's see what</Typography>
+             <Typography variant='p' style={{fontWeight: 'bold', fontSize: '30px'}}>you are looking</Typography>
+             <Typography variant='p' style={{fontWeight: 'bold', fontSize: '30px'}}>for!</Typography>
+            </>
+            }
 
+
+
+        </div>
     </div>
 
     </>
