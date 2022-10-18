bfa6abac3d5fed07699f2efc6ad23389ea94777c
Gravgor <36999577+Gravgor@users.noreply.github.com>
2022-10-18 02:50:29 +0200

    user UI welcome

--- project/src/components/app-user-login/user-login-ui/loginUI.js
+++ project/src/components/app-user-login/user-login-ui/loginUI.js
@@ -2,19 +2,22 @@ import React,{useState} from 'react'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import './loginUI.css'
 import { Typography } from '@mui/material';
+import { ReactComponent as Cooking} from './images/cooking1.svg';
+
 
 const LoginUI = () => {
 
     const [userStep, setUserStep] = useState(1)
     const [userName, setUserName] = useState('')
     const [userLocation, setUserLocation] = useState({})
+    const [userEndWelcome, setUserEndWelcome] = useState(false)
 
 
   return (
     <>
     <div className='login-header'>
         <div className='login-header-arrow'>
-            <ArrowForwardIcon style={{position: 'relative', top: '5px'}}/>
+            <ArrowForwardIcon id='icon' style={{position: 'relative', top: '5px'}} onClick={() => setUserStep((prev) => prev+1)}/>
         </div>
         <p>Skip</p>
     </div>
@@ -27,10 +30,15 @@ const LoginUI = () => {
              <Typography variant='p' style={{fontWeight: 'bold', fontSize: '30px'}}>you are looking</Typography>
              <Typography variant='p' style={{fontWeight: 'bold', fontSize: '30px'}}>for!</Typography>
             </>
-            }
-
-
-
+            }{userStep === 2 &&
+            <><p>podaj login</p></>}
+            {userStep === 3 &&
+            <><p>lokalizacja</p></>}
+            {userStep === 4 &&
+            <><p>zatwierdz wszystko</p></>}
+        </div>
+        <div className='login-image'>
+            <Cooking id='cooking'/>
         </div>
     </div>
 
